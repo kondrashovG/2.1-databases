4cc8ba981b15c69f293f7aa685da14cb8d04f4ce
kondrashovG <kondrashovg@gmail.com>
2023-01-30 23:37:34 +0300

    создание БД и импорт csv

---
+++ work_with_database/phones/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import Phone
+
+@admin.register(Phone)
+class PhoneAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'image', 'price', 'release_date', 'lte_exists', 'slug']
+
+# admin.site.register(Phone)
---
+++ work_with_database/phones/models.py
@@ -0,0 +1,16 @@
+from django.utils.text import slugify
+from django.db import models
+
+
+class Phone(models.Model):
+    # TODO: Добавьте требуемые поля
+    name = models.CharField(max_length=100)
+    price = models.IntegerField(default=0)
+    image = models.URLField(default='')
+    release_date = models.DateField(default=1)
+    lte_exists = models.BooleanField(default=False)
+    slug = models.SlugField(unique=True)
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super(Phone, self).save(*args, **kwargs)
