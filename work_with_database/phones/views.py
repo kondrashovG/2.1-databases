4276f6ae0e8c5cde34f8a529acabe1a60c18edf5
kondrashovG <kondrashovg@gmail.com>
2023-01-31 22:09:13 +0300

    окончательно

--- work_with_database/phones/views.py
+++ work_with_database/phones/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from .models import Phone
 
 
 def index(request):
@@ -7,11 +8,12 @@ def index(request):
 
 def show_catalog(request):
     template = 'catalog.html'
-    context = {}
+    context = {"phones": Phone.objects.all().order_by
+    ({'name': 'name', 'min_price': 'price', 'max_price': '-price', None: 'id'}[request.GET.get('sort')])}
     return render(request, template, context)
 
 
 def show_product(request, slug):
     template = 'product.html'
-    context = {}
+    context = {"phone": Phone.objects.get(slug=request.get_full_path().split('/')[-2])}
     return render(request, template, context)
